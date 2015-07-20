05b4a0a6ccc049efdfdd871c95b997df190f05d4
Carlos Rodrigues <carlos@jp7.com.br>
2015-07-20 10:02:06 +1000

    Add email controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'recommendations/new'
+
   root 'welcome#index'
   
   # get 'welcome/index'
 
   resources :authors
-  resources :articles
+  resources :articles do
+	resources :recommendations, only: [:new, :create]
+  end
 
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
